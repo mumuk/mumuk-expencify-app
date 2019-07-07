79141e918fad7f28cd8f5b4cb733d631eb69b5ef
Mykhailo Lytvynov <patrickarick@gmail.com>
2019-07-07 18:50:58 +0300

    add expencify styles

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -4,7 +4,7 @@ import {DateRangePicker} from 'react-dates';
 import {setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate} from '../actions/filters';
 
 //export class ExpenseListFilters extends React.Component { //для тестирования
- class ExpenseListFilters extends React.Component {
+class ExpenseListFilters extends React.Component {
   state = {
     calendarFocused: null
   };
@@ -32,27 +32,39 @@ import {setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate} from 
 
   render() {
     return (
-      <div>
-        <input
-          type="text"
-          value={this.props.filters.text}
-          onChange={this.onTextChange}/>
-        <select
-          value={this.props.filters.sortBy}
-          onChange={this.onSortChange}>
-          <option value="date">Date</option>
-          <option value="amount">Amount</option>
-        </select>
-        <DateRangePicker
-          startDate={this.props.filters.startDate}
-          endDate={this.props.filters.endDate}
-          onDatesChange={this.onDatesChange}
-          focusedInput={this.state.calendarFocused}
-          onFocusChange={this.onFocusChange}
-          showClearDates={true}
-          numberOfMonths={1}
-          isOutsideRange={(day) => false}
-        />
+      <div className="content-container">
+        <div className="input-group">
+          <div className="input-group__item">
+            <input
+              placeholder="Search expenses"
+              type="text"
+              className="text-input"
+              value={this.props.filters.text}
+              onChange={this.onTextChange}
+            />
+          </div>
+          <div className="input-group__item">
+            <select
+              className="select"
+              value={this.props.filters.sortBy}
+              onChange={this.onSortChange}>
+              <option value="date">Date</option>
+              <option value="amount">Amount</option>
+            </select>
+          </div>
+          <div className="input-group__item">
+            <DateRangePicker
+              startDate={this.props.filters.startDate}
+              endDate={this.props.filters.endDate}
+              onDatesChange={this.onDatesChange}
+              focusedInput={this.state.calendarFocused}
+              onFocusChange={this.onFocusChange}
+              showClearDates={true}
+              numberOfMonths={1}
+              isOutsideRange={(day) => false}
+            />
+          </div>
+        </div>
       </div>
     )
   }
---
+++ src/components/LoadingPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LoadingPage = () => (
+<div className="loader">
+  <img className="losder__image"
+    src="/images/loader.gif"/>
+</div>
+);
+
+export default LoadingPage;
